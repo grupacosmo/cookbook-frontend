00ebcfdf68654d11db3fb416fa6f2ae9deadfb61
PrzemyslawSk <36937448+PrzemyslawSk@users.noreply.github.com>
2022-02-24 15:11:02 +0100

    Minor changes#

--- src/views/RecipeView/Recipe.js
+++ src/views/RecipeView/Recipe.js
@@ -1,13 +1,16 @@
 import { Card } from 'antd';
 import { Row, Col } from 'antd';
+import { v4 } from 'uuid';
 
 function Recipe(props) {
-    const listItems = props.ingredientsList.map((ingredient) => <li>{ingredient}</li>);
+    const ingredientsItemsList = props.ingredientsList.map((ingredient) =>
+    <li key={v4()}>{ingredient}</li>
+    );
     return(
         <Row>
-            <Col span={8}></Col>
-            <Col span={8}>
-                <Card bodyStyle={{padding: 35}} style={{ width: 600}} >
+            <Col md={{span : 3}} lg={{span : 5}} xl={{span : 7}} xxl={{span : 8}}></Col>
+            <Col md={{span : 18}} lg={{span : 5}} xl={{span : 10}} xxl={{span : 8}}>
+                <Card bodyStyle={{padding: 35}} style={{width: 600}}>
 
                     <Col span={24}>
                         <h1>{props.title}</h1>
@@ -19,17 +22,17 @@ function Recipe(props) {
 
                     <Row>
                         <Col span={12}>
-                            {listItems}
+                            {ingredientsItemsList}
                         </Col>
                         <Col span={4}/>
                         <Col span={8}>
-                            <img style={{width: "100%"}} src={props.image} />
+                            <img style={{width: "100%"}} src={props.image} alt="Dish"/>
                         </Col>
                     </Row>
 
                 </Card>
             </Col>
-            <Col span={8}></Col>
+            <Col md={{span : 3}} lg={{span : 14}} xl={{span : 7}} xxl={{span : 8}}></Col>
         </Row>
     )
 }
--- src/views/RecipeView/RecipeView.js
+++ src/views/RecipeView/RecipeView.js
@@ -4,16 +4,20 @@ import Recipe from './Recipe';
 import {useParams} from "react-router-dom";
 
 const RecipeView = () => {
-    /* todo(przemyslaw.skoczewski)
-        Create simple layout with recipe (ingredients list, description, title, image)
-     */
-    const data = recipeData.map(recipe => <Recipe key={recipe.id} title={recipe.title}
-    description={recipe.description} ingredientsList={recipe.ingredientsList} image={recipe.image} path={recipe.id}/>)
+    const data = recipeData.map(recipe =>
+    <Recipe key={recipe.id}
+        title={recipe.title}
+        description={recipe.description}
+        ingredientsList={recipe.ingredientsList}
+        image={recipe.image}
+        path={recipe.id}
+        />
+    )
+    
     const { id } = useParams()
     return (
         <div>
             {data[id-1]}
-
         </div>
     )
 };
